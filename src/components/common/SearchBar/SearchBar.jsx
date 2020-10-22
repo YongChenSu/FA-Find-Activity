eb8e16bc57e9b16bb9d43956a3d88552df9a7efb
Yong <yongchensu0@gmail.com>
2020-10-22 16:35:11 +0800

    Add SearchBar component

---
+++ src/components/common/SearchBar/SearchBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <>
+      <form action="">
+        <input
+          className="searchBar"
+          type="text"
+          name="search"
+          placeholder="Find what you like" />
+      </form>
+    </>
+  )
+}
+
+export default SearchBar
