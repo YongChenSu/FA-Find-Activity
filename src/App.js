a6775dfc963fdb4371c4bb580a4ee371ca66ae90
Yong <yongchensu0@gmail.com>
2020-10-22 16:28:25 +0800

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState } from 'react'
+import Routes from './routes'
+import { BrowserRouter as Router } from 'react-router-dom'
+import 'normalize.css/normalize.css'
+import './styles/main.sass'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes />
+    </Router>
+  )
 }
 
-export default App;
+export default App
