37376505243ce8ca0c8e125de26e97a6c9011d84
YongChenSu <yongchensu0@gmail.com>
2020-10-23 19:04:45 +0800

    refactor sass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component, useState } from 'react'
 import Routes from './routes'
 import { BrowserRouter as Router } from 'react-router-dom'
-import 'normalize.css/normalize.css'
 import './styles/main.sass'
 
 const App = () => {
