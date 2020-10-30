2d4f5717337feb841b1a6cc913b0832a96ce81bf
MSI\USER <yongchensu0@gmail.com>
2020-10-30 21:01:43 +0800

    update Button component

--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -1,39 +1,21 @@
-import React from 'react';
 import styled from '@emotion/styled'
-// const Button = ({
-//   text,
-//   id,
-//   onClick
-// }) => {
-//   return (
-//     <button className="basic-btn" id={id} onClick={onClick}>{text}</button>
-//   )
-// }
-
-
-
 
 const Button = styled.button`
   font-size: 1.25rem;
   font-weight: 500;
   letter-spacing: 1px;
-  color: ${({ theme }) => theme.$colorWhite};
-  background-color: ${({ theme }) => theme.$colorRed};
+  color: ${({ theme }) => theme.main.$colorRed};
   padding: 0.25rem 0.5rem;
   border: none;
-  border-radius: ${({ theme }) => theme.$borderRadius };
+  border-radius: ${({ theme }) => theme.main.$borderRadius };
   opacity: 0.8;
   position: relative;
   cursor: pointer;
-  transition-duration: 0.25s;
+  transition-duration: 0.3s;
   margin: 0 0.25rem;
-  &:hover {
-    background-color: darken(${({ theme }) => theme.$colorWhite}, 8%);
-    opacity: 1;
-    transform: scale(1.05);
-  }
+  border: 1.5px solid ${({ theme }) => theme.main.$colorRed};
+
   &:focus {
-    box-shadow: 0 0 1px 2.5px rgba(${({ theme }) => theme.$colorWhite}, .5);
     z-index: 1;
     outline: 0;
   }
