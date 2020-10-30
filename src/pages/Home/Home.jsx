3e0808c3ee2e9a8daf5141a2073ac9c9a8fef465
MSI\USER <yongchensu0@gmail.com>
2020-10-30 21:02:04 +0800

    update Home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,15 @@
-import React, { Component, useState } from 'react'
+import React from 'react'
 import Header from '../../components/common/Header'
 import Footer from '../../components/common/Footer'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import Category from '../../components/common/Category'
 import Button from '../../components/common/Button'
-import logoImg from '../../assets/img/FA_logo.png'
 import theme from '../../styles/base/variable.js'
 import styled from '@emotion/styled'
 import { ThemeProvider } from 'emotion-theming'
-import { keyframes } from '@emotion/core'
 import ActCard from '../../components/common/ActCard'
-// import { keyframes } from "styled-components"
+import { FaAngleDoubleRight } from 'react-icons/fa'
 
 const HomeContainer = styled.div`
+  font-family: ${({ theme}) => theme.main.$fontFamily};
 `
 
 const Carousel = styled.div`
@@ -22,13 +19,12 @@ const Carousel = styled.div`
   text-align: center;
   width: 100%;
   height: 500px;
-  background-color: ${({ theme }) => theme.$colorYellow };
+  background-color: ${({ theme }) => theme.main.$colorYellow };
   border: 1px solid #000;
 `
 
 const ActContainer = styled.div`
   max-width: 1200px;
-  font-family: ${({ theme }) => theme.$fontFamily};
   margin: 0 auto;
   display: flex;
   flex-wrap: wrap;
@@ -36,6 +32,30 @@ const ActContainer = styled.div`
   padding: 2rem 0;
 `
 
+const MoreActButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 0 2rem 0;
+  height: 3rem;
+`
+
+const MoreActButton = styled(Button)`
+  width: 330px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+  &:hover {
+    color: ${({ theme }) => theme.main.$colorWhite};
+    box-shadow: inset 22rem 0 0 0 ${({ theme }) => theme.main.$colorRed};
+    border: 0;
+  }
+
+  svg {
+    margin: 0 0.5rem;
+  }
+`
 
 const Home = () => {
   return (
@@ -55,6 +75,12 @@ const Home = () => {
               <ActCard />
               <ActCard />
             </ActContainer>
+            <MoreActButtonContainer>
+              <MoreActButton>
+                尋找更多活動
+                <FaAngleDoubleRight />
+              </MoreActButton>
+            </MoreActButtonContainer>
           <Footer />
         </HomeContainer>
       </ThemeProvider>
