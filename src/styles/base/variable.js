5fdea2e3572fd716b0f2abc58f702918172f3dec
MSI\USER <yongchensu0@gmail.com>
2020-10-30 20:59:22 +0800

    update variable.js: set main variable

--- src/styles/base/variable.js
+++ src/styles/base/variable.js
@@ -15,17 +15,20 @@
 // }
 
 const theme = {
-  $colorWhite: "#f8f8f8",
-  $colorRed: "#E6756B",
-  $colorLightGreen: "#C4C88F",
-  $colorGreen: "#8b8974",
-  $colorYellow: "#F9D893",
+  main: {
+    $colorWhite: "#f8f8f8",
+    $colorRed: "#e6756B",
+    $colorLightGreen: "#c4c88f",
+    $colorGreen: "#8b8974",
+    $colorLightOrange: "#fae1d2",
+    $colorYellow: "#F9d893",
 
-  $colorGrey: "#777",
-  $colorLightGrey: "#aaa",
+    $colorGrey: "#777",
+    $colorLightGrey: "#aaa",
 
-  $borderRadius: "0.5rem",
-  $fontFamily: "'Varela Round', 'cwTeXYen'",
+    $borderRadius: "0.5rem",
+    $fontFamily: "'Varela Round', 'cwTeXYen'",
+  },
 }
 
 export default theme
