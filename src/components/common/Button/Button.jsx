9b5c2974e3dbeeef1b29143d397ea38e0e3a4310
MSI\USER <yongchensu0@gmail.com>
2020-10-31 19:38:36 +0800

    Update Button component

--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -4,16 +4,16 @@ const Button = styled.button`
   font-size: 1.25rem;
   font-weight: 500;
   letter-spacing: 1px;
-  color: ${({ theme }) => theme.main.$colorRed};
+  color: ${({ theme }) => theme.$colorRed};
   padding: 0.25rem 0.5rem;
   border: none;
-  border-radius: ${({ theme }) => theme.main.$borderRadius };
+  border-radius: ${({ theme }) => theme.$borderRadius };
   opacity: 0.8;
   position: relative;
   cursor: pointer;
   transition-duration: 0.3s;
   margin: 0 0.25rem;
-  border: 1.5px solid ${({ theme }) => theme.main.$colorRed};
+  border: 1.5px solid ${({ theme }) => theme.$colorRed};
 
   &:focus {
     z-index: 1;
