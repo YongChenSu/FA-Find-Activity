8a0b15a5e221c4841846488ab8d55df5c6a297b2
MSI\USER <yongchensu0@gmail.com>
2020-10-31 19:39:04 +0800

    Update Category component

--- src/components/common/Category/Category.jsx
+++ src/components/common/Category/Category.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaUserFriends } from 'react-icons/fa'
+import styled from '@emotion/styled'
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  padding: 2rem 4rem;
+`
+
 
 const Category = () => {
   return (
     <div id="category">
-      <Link to="activity" className="category__item">聯誼</Link>
+      <Link to="activity" className="category__item"><FaUserFriends />聯誼</Link>
       <Link to="activity" className="category__item">講座</Link>
       <Link to="activity" className="category__item">旅行</Link>
       <Link to="activity" className="category__item">展覽</Link>
