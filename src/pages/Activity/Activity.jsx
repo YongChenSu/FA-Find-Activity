a74ebc65db94731fe7e2b0b26bf2ba85e5ce0012
MSI\USER <yongchensu0@gmail.com>
2020-11-02 18:38:17 +0800

    Update Activity component with adding getWeatherData function

--- src/pages/Activity/Activity.jsx
+++ src/pages/Activity/Activity.jsx
@@ -5,6 +5,10 @@ import { ThemeProvider } from 'emotion-theming'
 import Button from '../../components/common/Button'
 import Header from '../../components/common/Header'
 import Footer from '../../components/common/Footer'
+import dayjs from 'dayjs';
+import { TiWeatherPartlySunny } from 'react-icons/ti'
+import { WiStrongWind } from 'react-icons/wi'
+import { IoIosUmbrella } from 'react-icons/io'
 
 const ActivityContainer = styled.div`
   font-family: ${({ theme}) => theme.$fontFamily};
@@ -45,26 +49,76 @@ const Title = styled.div`
   font-weight: bold;
 `
 
-const Weather = styled.div`
+const WeatherContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 1rem 0 2rem 0;
+`
+
+const WeatherCard = styled.div`
+  color: ${({ theme }) => theme.$colorGreen};
+  width: 200px;
+  border: 1px solid ${({ theme }) => theme.$colorGreen};
+  box-shadow: 1px 1px 1px 2px rgba(0, 0, 0, 0.1);
+  margin: 0 0.25rem;
+  border-radius: ${({ theme }) => theme.$borderRadius };
+  padding: 0.5rem;
+`
+
+const Location = styled.div`
+  font-size: 2rem;
+`
+
+const Description = styled.div`
+  font-size: 1rem;
+`
+
+const CurrentWeather = styled.div`
   display: flex;
   justify-content: center;
-  margin: 1rem 0 2  rem 0;
+  margin: 0.5rem 0;
+  svg {
+    width: 3.25rem;
+    height: 3.25rem;
+  }
 `
 
-const Card = styled.div`
-  height: 200px;
-  width: 100%;
-  background-color: ${({ theme }) => theme.$colorLightGreen};
-  border: 1px solid grey;
-  margin: 0 0.5rem;
-  border-radius: ${({ theme }) => theme.$borderRadius };
-  text-align: center;
-  line-height: 200px;
+const Temperature = styled.div`
+  font-size: 3.25rem;
+`;
+
+const Celsius = styled.div`
+  font-size: 1rem;
+`;
+
+const AirFlow = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 0.5rem;
+  font-weight: 300;
+
+  svg {
+    width: 1.5rem;
+    height: 1.5rem;
+    margin: 0 0.5rem 0 0;
+  }
 `
 
-const Activity = () => {
-  const [currentTheme, setCurrentTheme] = useState('main')
-  const title = '  涼州詞王翰 - 古詩誦賞'
+const Rain = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 0.5rem;
+  font-weight: 300;
+
+  svg {
+    width: 1.5rem;
+    height: 1.5rem;
+    margin: 0 0.5rem 0 0;
+  }
+`
+
+const title = '  涼州詞王翰 - 古詩誦賞'
   const tag = '美食'
   const time = '2020/11/01'
   const location = '臺北市松山區'
@@ -77,6 +131,51 @@ const Activity = () => {
     location,
     introduction,
   }
+
+const BASE_URL = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization'
+const AUTHORIZATION_KEY = 'CWB-15DFF2FC-FFFB-49E9-BF7F-EBB9164F4B47'
+const LOCATION_NAME = '臺北'
+
+const Activity = () => {
+  const [currentTheme, setCurrentTheme] = useState('main')  
+  const [currentWeather, setCurrentWeather] = useState({
+    observationTime: '2020-12-12 22:10:00',
+    locationName: '臺北市',
+    description: '晴天',
+    windSpeed: 5,
+    temperature: 25,
+    rainPossibility: 20,
+  });
+
+  const getWeatherData = () => {
+    fetch(
+      `${BASE_URL}=${AUTHORIZATION_KEY}&locationName=${LOCATION_NAME}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const locationData = data.records.location[0]
+        const weatherElements = locationData.weatherElement.reduce(
+          (neededElements, item) => {
+            if (['WDSD', 'TEMP'].includes(item.elementName)) {
+              neededElements[item.elementName] = item.elementValue
+            }
+            return neededElements
+          },
+          {}
+        )
+
+        setCurrentWeather({
+          observationTime: locationData.time.obsTime,
+          locationName: locationData.locationName,
+          temperature: weatherElements.TEMP,
+          windSpeed: weatherElements.WDSD,
+          description: '大晴天',
+          rainPossibility: 10,
+        });
+      })
+  }
+
+  getWeatherData()
 
   return (
     <>
@@ -104,13 +203,23 @@ const Activity = () => {
                 <li>{actInfo.introduction}</li>
               </ul>
             </ActInfoContainer>
-            <Weather>
-              <Card>MON</Card>
-              <Card>TUE</Card>
-              <Card>WED</Card>
-              <Card>THU</Card>
-              <Card>FRI</Card>
-            </Weather>
+            <WeatherContainer>
+              <WeatherCard>
+                <Location>{currentWeather.locationName}</Location>
+                <Description>{currentWeather.description}</Description>
+                <CurrentWeather>
+                  <Temperature>{Math.round(currentWeather.temperature)}</Temperature>
+                  <Celsius>°C</Celsius>
+                  <TiWeatherPartlySunny />
+                </CurrentWeather>
+                <AirFlow>
+                  <WiStrongWind />{currentWeather.windSpeed} m/h
+                </AirFlow>
+                <Rain>
+                  <IoIosUmbrella />{currentWeather.rainPossibility}%
+                </Rain>
+              </WeatherCard>
+            </WeatherContainer>
           </ActivityWrapper>
         </ActivityContainer>
         <Footer />
