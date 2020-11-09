0e62e2d9609a578cf8ae1e295ceed1c49ebe8129
MSI\USER <yongchensu0@gmail.com>
2020-11-09 18:50:27 +0800

    Update Router and pahtname

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { Component, useState } from 'react'
-import Routes from './routes'
-import { BrowserRouter as Router } from 'react-router-dom'
-import './styles/main.sass'
-
+import React, { Component, useState } from "react";
+import Routes from "./routes";
+import { HashRouter as Router } from "react-router-dom";
+import "./styles/main.sass";
 
 const App = () => {
   return (
     <Router>
       <Routes />
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
