3a73f769c0db9584263bfde94be6b09b450f0347
MSI\USER <yongchensu0@gmail.com>
2020-11-09 18:57:07 +0800

    Delete React.StricMode & Add ThemeProvider

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { ThemeProvider } from "emotion-theming";
+import theme from "./styles/base/variable";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme.main}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ThemeProvider>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
