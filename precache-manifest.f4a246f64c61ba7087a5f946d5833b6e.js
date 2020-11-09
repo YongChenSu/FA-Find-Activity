468fe6bc690b7df83582b51aa8795c40045a5e88
MSI\USER <yongchensu0@gmail.com>
2020-11-09 13:33:25 +0800

    Updates

---
+++ precache-manifest.f4a246f64c61ba7087a5f946d5833b6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d5737ed189cee0e78ab266364e455f",
+    "url": "/FA-Find-Activity/index.html"
+  },
+  {
+    "revision": "a0dfc0bd6beb45faa0de",
+    "url": "/FA-Find-Activity/static/css/main.be047d9e.chunk.css"
+  },
+  {
+    "revision": "53a8de45a39fdf654761",
+    "url": "/FA-Find-Activity/static/js/2.cb82076f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/FA-Find-Activity/static/js/2.cb82076f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0dfc0bd6beb45faa0de",
+    "url": "/FA-Find-Activity/static/js/main.14c67a50.chunk.js"
+  },
+  {
+    "revision": "9afc49d280ff25e948f4",
+    "url": "/FA-Find-Activity/static/js/runtime-main.811ed77a.js"
+  },
+  {
+    "revision": "109fd7f476d23283e16794dcd8d1ff55",
+    "url": "/FA-Find-Activity/static/media/FA_logo.109fd7f4.png"
+  },
+  {
+    "revision": "683974183f0893ae2fae94007887ffdb",
+    "url": "/FA-Find-Activity/static/media/FA_logo_bgwhite.68397418.png"
+  }
+]);
