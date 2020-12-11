6aa904263574ad4cae33023f09b2892888b92e24
YongChenSu <yongchensu0@gmail.com>
2020-12-11 17:01:06 +0800

    Delete uncompleted pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,7 @@ import { HashRouter as Switch, Route } from "react-router-dom";
 const Home = lazy(() => import("../pages/Home"));
 const FindActivity = lazy(() => import("../pages/FindActivity"));
 const Activity = lazy(() => import("../pages/Activity"));
-const NewActivity = lazy(() => import("../pages/NewActivity"));
 const Login = lazy(() => import("../pages/Login"));
-const Profile = lazy(() => import("../pages/Profile"));
 
 const Routes = () => {
   return (
@@ -14,9 +12,7 @@ const Routes = () => {
         <Route exact path="/" component={Home} />
         <Route path="/find-activity" component={FindActivity} />
         <Route path="/activity/:id" component={Activity} />
-        <Route path="/new-activity" component={NewActivity} />
         <Route path="/login-page" component={Login} />
-        <Route path="/profile-page" component={Profile} />
       </Switch>
     </Suspense>
   );
