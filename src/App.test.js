ec46d2f81508cbed43b004e1d9684ac79601b0a6
YongChenSu <yongchensu0@gmail.com>
2020-12-11 10:59:19 +0800

    Text jest

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/美食/i);
+  const linkElement = getByText(/講座/i);
   expect(linkElement).toBeInTheDocument();
 });
